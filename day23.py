2b9606c366d33e7b622d2f3b633eb337c2e117ea
de_shrike@hotmail.com <de_shrike@hotmail.com>
2020-12-26 11:57:32 +0000

    Day 23 Part 2 done at last

--- day23.py
+++ day23.py
@@ -19,108 +19,222 @@ def TestData():
 #########################################
 #########################################
 
-def Remove3CW(numbers, current):
-	pos = numbers.index(current) + 1
-	if pos < len(numbers) - 3:
-		c1 = numbers[pos]
-		c2 = numbers[pos + 1]
-		c3 = numbers[pos + 2]
-		numbers = numbers[:pos] + numbers[pos + 3:]
-	else:	
-		c1 = RemoveCW(numbers, current)
-		c2 = RemoveCW(numbers, current)
-		c3 = RemoveCW(numbers, current)
-	
-	return (c1, c2, c3)
+class Item():
+	def __init__(self, value: int):
+		self.value = value
+		self.next = None
+		self.prev = None
 
-def RemoveCW(numbers, current):
-	pos = numbers.index(current) + 1
-	if pos < len(numbers):
-		num = numbers[pos]
-		numbers.pop(pos)
-		return num
-	else:
-		num = numbers[0]
-		numbers.pop(0)
-		return num
+class CircularLinkedList():
+	def __init__(self):
+		self.list = None
+		self.length = 0
+		self.current = None
+		self.saved_current = None
+		self.dict = {}
 
-def ShowNumbers(numbers, current):
-	for num in numbers:
-		if num == current:
-			print(BrightYellow, end = "")
-		print(f"{num}{Reset} ", end = "")
-	print("")
+	def Length(self) -> int:
+		return self.length
 
-def ShowNumbersEx(numbers, current):
-	for num in numbers:
-		if num == current:
-			print(f"({num}) ", end = "")
+	def Current(self):
+		return self.current
+
+	def CurrentValue(self) -> int:
+		return self.current.value
+
+	def SaveCurrent(self):
+		self.saved_current = self.current
+
+	def RestoreCurrent(self):
+		self.current = self.saved_current
+
+	def MoveClockwise(self, count: int = 1):
+		for _ in range(count):
+			self.current = self.current.next
+
+	def MoveCounterClockwise(self, count: int = 1):
+		for _ in range(count):
+			self.current = self.current.prev
+
+	def MoveToValue(self, value: int) -> bool:
+		item = self.dict[value]
+		if item is None:
+			return False
+		self.current = item
+		return True
+
+	def GetItemByOffset(self, offset: int):
+		item = self.current
+		if offset < 0:
+			for _ in range(abs(offset)):
+				item = item.pref
+		elif offset > 0:
+			for _ in range(offset):
+				item = item.next
+		return item
+
+	def Remove(self, offset: int = 0):
+		if self.length == 1:
+			item = self.current
+			self.dict[item.value] = None
+			self.list = None
+			self.current = None
+			self.length -= 1
+			return item
+
+		if offset == 0:
+			item = self.current
+			self.dict[item.value] = None
+			if item == self.list:
+				self.list = item.prev
+			self.current = self.current.next
+			item.prev.next = self.current
+			self.current.prev = item.prev
+			self.length -= 1
+			item.next = item.prev = None
+			return item
 		else:
-			print(f"{num} ", end = "")
-	print("")
+			item = self.GetItemByOffset(offset)
+			self.dict[item.value] = None
+			if item == self.list:
+				self.list = item.prev
+			item.prev.next = item.next
+			item.next.prev = item.prev
+			self.length -= 1
+			item.next = item.prev = None
+			return item
 
-def Play(numbers, turns: int):
-	last_n1 = 0
-	last_n2 = 0
-	fullen = len(numbers)
-	current = numbers[0]
-	for i in range(turns):
-		#print(f"{i}", end = "\r")
-		# ShowNumbers(numbers, current)
-		#c1, c2, c3 = Remove3CW(numbers, current)
-		c1 = RemoveCW(numbers, current)
-		c2 = RemoveCW(numbers, current)
-		c3 = RemoveCW(numbers, current)
-		# print(f"Pickup {c1} {c2} {c3}")
-		# ShowNumbers(numbers, current)
-		dest = current - 1
-		while dest not in numbers:
-			dest -= 1
-			if dest <= 0:
-				dest = max(numbers)
-		destix = numbers.index(dest)
-		# print(f"Target: {dest} {destix}")
-		newnumbers = numbers[:destix + 1]
-		newnumbers.extend([c1, c2, c3])
-		newnumbers.extend(numbers[destix + 1:])
-		numbers = newnumbers
-		cix = numbers.index(current)
-		cix = (cix + 1) % fullen
-		current = numbers[cix]
-		# ShowNumbers(numbers, current)
-		# qq = input()
-		# print("")
-		if True: #(i + 1) % 1000 == 0:
-			ix1 = numbers.index(1)
-			if ix1 < fullen - 2:
-				n1 = numbers[ix1 + 1]
-				n2 = numbers[ix1 + 2]
-				if n1 != last_n1 or n2 != last_n2:
-					print(f"Turn {i + 1} Index of 1 = {ix1} Numbers: {n1} and {n2}")
-					last_n1 = n1
-					last_n2 = n2
+	def Insert(self, value):
+		item = Item(value)
+		self.dict[value] = item
+		if self.list is None:
+			self.list = item
+			item.next = item
+			item.prev = item
+			self.current = item
+			self.length += 1
+		else:
+			# 1 <-> 2 <-> C <-> I <-> 3 <-> F
+			curr = self.current
+			prev = curr
+			nexxt = curr.next
 
-	#ShowNumbersEx(numbers, current)
+			prev.next = item
+			item.prev = prev
+			item.next = nexxt
+			nexxt.prev = item
+			self.current = item
+			self.length += 1
 
-	answerA = ""
-	ix = numbers.index(1) + 1
-	while numbers[ix] != 1:
-		answerA += str(numbers[ix])
-		ix = (ix + 1) % len(numbers)
+	def Find(self, value: int):
+		return self.dict[value]
+		"""
+		item = self.current
+		while item.value != value:
+			item = item.prev
+			if item == self.current:
+				return None
+		return item
+		"""
 
-	ix1 = numbers.index(1)
-	answerB = numbers[ix1 + 1] * numbers[ix1 + 2]
-	return answerA, answerB
+	def MaxValue(self) -> int:
+		m = 0
+		item = self.current
+		while True:
+			m = max(m, item.value)
+			item = item.next
+			if item == self.current:
+				break
+		return m
+
+	def Print(self):
+		item = self.list
+		while True:
+			if item == self.current:
+				print(f"{BrightYellow}{item.value}{Reset} -> ", end = "")
+			else:
+				print(f"{item.value} -> ", end = "")
+			item = item.next
+			if item == self.list:
+				break
+		print("")
+
+		"""
+		item = self.list
+		while True:
+			if item == self.current:
+				print(f"{BrightYellow}{item.value}{Reset} -> ", end = "")
+			else:
+				print(f"{item.value} -> ", end = "")
+			item = item.prev
+			if item == self.list:
+				break
+		print("")
+		"""
+
+#########################################
+#########################################
+
+def Play(ll, turns: int):
+	for turn in range(turns):
+		if turn % 1000 == 0:
+			print(turn, end = "\r")
+		# ll.Print()
+		#print("Removing")
+		c1 = ll.Remove(1).value
+		c2 = ll.Remove(1).value
+		c3 = ll.Remove(1).value
+		# print(f"Picked up: {c1}, {c2}, {c3}")
+		targetitem = None
+		targetvalue = ll.Current().value
+		while targetitem is None:
+			targetvalue -= 1
+			if targetvalue == 0:
+				#print("MaxValue")
+				targetvalue = ll.MaxValue()
+			#print("Finding")
+			targetitem = ll.Find(targetvalue)
+		# print(f"Target: {targetvalue}")
+		ll.SaveCurrent()
+		#print("MovingTo")
+		ll.MoveToValue(targetvalue)
+		#print("Inserting")
+		ll.Insert(c1)
+		ll.Insert(c2)
+		ll.Insert(c3)
+		ll.RestoreCurrent()
+		ll.MoveClockwise()
+
+	answera = ""
+	ll.MoveToValue(1)
+	ll.MoveClockwise()
+	while ll.CurrentValue() != 1:
+		answera += str(ll.CurrentValue())
+		ll.MoveClockwise()
+
+	ll.MoveToValue(1)
+	ll.MoveClockwise()
+	v1 = ll.CurrentValue()
+	ll.MoveClockwise()
+	v2 = ll.CurrentValue()
+	answerb = v1 * v2
+
+	return answera, answerb
 
 #########################################
 #########################################
 
 def PartA():
 	StartPartA()
-	# TestData()
+	#TestData()
 
-	numbers = [int(c) for c in inputdata[0]]
-	answer, _ = Play(numbers, 100)
+	ll = CircularLinkedList()	
+	for c in inputdata[0]:
+		ll.Insert(int(c))
+	ll.MoveToValue(int(inputdata[0][0]))
+
+	answer, _ = Play(ll, 100)
+
 	ShowAnswer(answer)
 
 #########################################
@@ -128,13 +242,21 @@ def PartA():
 
 def PartB():
 	StartPartB()
-	# TestData()
+	#TestData()
 
-	numbers = [int(c) for c in inputdata[0]]
-	m = max(numbers) + 1
-	for num in range(m, 1_000 + 1):			# 1_000_000
-		numbers.append(num)
-	_, answer, = Play(numbers, 10_000)   # 10_000_000
+	ll = CircularLinkedList()	
+	for c in inputdata[0]:
+		ll.Insert(int(c))
+
+	print("Creating Circular Linked List")
+	m = ll.MaxValue() + 1
+	for num in range(m, 1_000_000 + 1):			# 1_000_000
+		ll.Insert(num)
+
+	ll.MoveToValue(int(inputdata[0][0]))
+	print("Starting Game")
+	_, answer, = Play(ll, 10_000_000)   # 10_000_000
+
 	ShowAnswer(answer)
 
 #########################################
